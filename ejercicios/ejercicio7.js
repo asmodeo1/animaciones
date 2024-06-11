13248ce0dc36dc4c82e3343d31d88735510b39b0
Juan <alonso.jad@gmail.com>
2024-06-11 14:00:19 +0200

    añadidas soluciones ejercicios 1, 2, 3, 4, 5 y 6

---
+++ ejercicios/ejercicio7.js
@@ -0,0 +1,10 @@
+function moverX() {
+
+}
+
+function moverY() {
+    
+}
+
+document.getElementById("moverX").addEventListener("click", moverX);
+document.getElementById("moverY").addEventListener("click", moverY);
